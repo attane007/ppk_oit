c16d15af7b0f652783a70ec4ea1dd9fb3aecf0c0
FXtreme <m10226677@gmail.com>
2021-11-25 05:53:53 +0700

    add icon

--- src/main.js
+++ src/main.js
@@ -3,4 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
+// import css file
+import '@/assets/css/nucleo-icons.css'
+import '@/assets/css/nucleo-svg.css'
+import '@/assets/css/soft-ui-dashboard.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+// import js
+require('@/assets/js/core/popper.min.js')
+import "bootstrap"
+// import '@/assets/js/plugins/perfect-scrollbar.min.js'
+// import '@/assets/js/plugins/smooth-scrollbar.min.js'
+// require('@/assets/js/plugins/chartjs.min.js')
+
+
+
 createApp(App).use(store).use(router).mount('#app')
